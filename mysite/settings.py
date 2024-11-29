fb62e275f21adc9f2b6f02b2a8da647e1a6b0462
Valentina Chandia <artemisiaklartraum@gmail.com>
2024-11-28 21:52:31 -0300

    se agrego Static_root

--- mysite/settings.py
+++ mysite/settings.py
@@ -133,6 +133,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
